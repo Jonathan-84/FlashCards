a6af0e3871ba548f87650b796ad132a7db2c18ef
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-10-13 15:58:11 -0400

    all basics done, need style math and reward

--- src/Pages/basicaddition.js
+++ src/Pages/basicaddition.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
+export default class newaddition extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      // This is a default value...
+      firstnumber: [0],
+      secondnumber:[0]
+
+    }
+  }
+
+  generateNumbers = () => {
+    var randomNumber1 = Math.floor(Math.random() * 10) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 10) + 1;
 
 
-function newaddition(){
+    this.setState({ firstnumber: randomNumber1,secondnumber:randomNumber2 })
+  }
 
+
+  render() {
+
+    const {firstnumber,secondnumber} = this.state;
+    
     return (
-<h2>New Addition</h2>
-    )
-    };
-  export default newaddition;
+   <div>
+        <h2>New Addition</h2>
+
+        <button title="Generate Random Number" onClick={this.generateNumbers}>Press Me</button> 
+        <h2>{firstnumber} + {secondnumber}</h2>
+
+        </div>
+    );
+  }
+}
+
+
+
--- src/Pages/basicsubtraction.js
+++ src/Pages/basicsubtraction.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
+export default class newsubtraction extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      // This is a default value...
+      firstnumber: [0],
+      secondnumber:[0]
+
+    }
+  }
+
+  generateNumbers = () => {
+    var randomNumber1 = Math.floor(Math.random() * 10) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 10) + 1;
 
 
-function newsubtraction(){
+    this.setState({ firstnumber: randomNumber1,secondnumber:randomNumber2 })
+  }
 
+
+  render() {
+
+    const {firstnumber,secondnumber} = this.state;
+
+    console.log (firstnumber,secondnumber);
+
+    let subtraction;
+
+    if 
+    (firstnumber >= secondnumber) {
+      subtraction = (
+        <h2>{firstnumber} - {secondnumber}</h2>
+      )
+    }
+    else{
+      subtraction=(
+        <h2>{secondnumber} - {firstnumber}</h2>
+     )
+    }
+    
     return (
-<h2>New Subtraction</h2>
-    )
-    };
-  export default newsubtraction;
+   <div>
+        <h2>New Subtraction</h2>
+
+        <button title="Generate Random Number" onClick={this.generateNumbers}>Press Me</button> 
+        
+        {subtraction}
+
+        </div>
+    );
+  }
+}
