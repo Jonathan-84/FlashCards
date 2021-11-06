ac275375d92e783375cdac2be87fc24d14a67f6c
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-11-06 10:47:20 -0400

    fixed the fit for the mobile home page

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -12,7 +12,7 @@ function Home(){
         <div className='col'>
         
 <h2 className='text-center'>FlashCards</h2>
-<div className='col'>
+<div className='col '>
         <div className="alert alert-primary text-center" role="alert">
   Future Updates: Convert to a Progressive Web App and the ability to input answers which will instantly
   be graded and move the tracker. More topics will be added, after all, I created this as an aid for my kids as well.
@@ -38,10 +38,10 @@ function Home(){
 <br></br>
 <br></br>
 
-<div className="container-fluid col-6 border border-success rounded ">
-<div className=" row mr-auto d-flex flex-wrap justify-content-center align-items-center">
+<div className="container-fluid col-7 border border-success rounded p-1 ">
+<div className=" row  d-flex flex-wrap justify-content-center align-items-center">
   <div className='col'>
-<img className='align-left  p-1'src='https://raw.githubusercontent.com/Jonathan-84/FlashCards/master/src/Assets/FlashCardsQR.png' alt='QR code to site'
+<img className='align-left p-1'src='https://raw.githubusercontent.com/Jonathan-84/FlashCards/master/src/Assets/FlashCardsQR.png' alt='QR code to site'
 height={200} width={200} />
 </div>
 <div className='col'>
