8c92abf2869661319051a4a32967809ff3f02875
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-11-06 08:46:02 -0400

    updated the QR code and format

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -12,6 +12,12 @@ function Home(){
         <div className='col'>
         
 <h2 className='text-center'>FlashCards</h2>
+<div className='col'>
+        <div className="alert alert-primary text-center" role="alert">
+  Future Updates: Convert to a Progressive Web App and the ability to input answers which will instantly
+  be graded and move the tracker. More topics will be added, after all, I created this as an aid for my kids as well.
+</div>
+</div>
 <br></br>
 <div className="d-flex mt-auto justify-content-center">
 <p> This simple to use site will help early learners practice their letters, numbers, and math. Following each 
@@ -28,23 +34,26 @@ function Home(){
     
     The goal of this simple site was to give repeatable practice opportunities and make it fun.
 </p>
-<span>
-<img src='https://raw.githubusercontent.com/Jonathan-84/FlashCards/master/src/Assets/FlashCardsQR.png' alt='QR code to site'
-height={200} width={200} />
-<p className='text-center'>Share This App</p>
-</span>
 </div>
 <br></br>
-<div>
-  <div></div>
+<br></br>
+
+<div className="container-fluid col-6 border border-success rounded ">
+<div className=" row mr-auto d-flex flex-wrap justify-content-center align-items-center">
+  <div className='col'>
+<img className='align-left  p-1'src='https://raw.githubusercontent.com/Jonathan-84/FlashCards/master/src/Assets/FlashCardsQR.png' alt='QR code to site'
+height={200} width={200} />
+</div>
+<div className='col'>
+<p className='text-center p-1'>Share This App: If you like the application, share it with others by letting them
+scan this QR code. </p>
+</div>
+</div>
 </div>
 
-<div className='col'>
-        <div className="alert alert-primary text-center" role="alert">
-  Future Updates: Convert to a Progressive Web App and the ability to input answers which will instantly
-  be graded and move tracker... to start.
-  the line. 
-</div>
+<br></br>
+<div>
+
 </div>
 <br></br>
 
