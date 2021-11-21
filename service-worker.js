fd46abcf073b38e347e142ab35f6b7f2cde62e88
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-11-21 10:44:41 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,46 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable no-restricted-globals */
-const APP_PREFIX = 'FlashCards-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
-const FILES_TO_CACHE = [
-    './index.html',
-];
+const CACHE_NAME='version-1'
+const urls=['index.html','offline.html']
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-});
+const self=this
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX)
+//Install SW
+self.addEventListener('install',(event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened Cache')
+
+                return cache.addAll(urls)
             })
-
-            cacheKeeplist.push(CACHE_NAME);
-
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i]);
-                    return caches.delete(keyList[i]);
-                }
-            }));
-        })
-    );
-});
-
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
     )
-});
+})
+
+//Listen for requests
+
+self.addEventListener('fetch',(event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then( () => {
+                return fetch(event.request)
+                    .catch( () => caches.match('offline.html'))
+            })
+    )
+})
+
+
+//Activating SW
+
+self.addEventListener('activate', (event) =>{
+    const cacheWhitelist=[]
+    cacheWhitelist.push(CACHE_NAME)
+
+    event.waitUntil(
+        caches.keys()
+            .then( (cacheNames) => Promise.all(
+                cacheNames.map((cacheName) => {
+                    if(!cacheWhitelist.includes(cacheName))
+                        return caches.delete(cacheName)
+                })
+            ))
+    )
+})
